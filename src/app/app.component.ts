8d7fff9b56cc21339bd0c61964ebc08de1617228
Thibault Sanclemente <thibault_sanclemente@icloud.com>
2023-07-03 13:41:05 +0200

    Ajout composants et réorganisation du code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CV';
-  // Centres interet
-  imageAvion = "https://cdn3.iconfinder.com/data/icons/avion/512/plane-1024.png"
-  imageManette = "https://cdn4.iconfinder.com/data/icons/outline-phone-3/512/j-01-1024.png"
-  imageBallon = "https://cdn4.iconfinder.com/data/icons/footbal-element/30/ball-1024.png"
-  imageRando = "https://cdn1.iconfinder.com/data/icons/hobby-vol-2-innovicons-black-and-white/128/climbing-mountain-alpinism-flag-1024.png"
-// Image profil
-  imageprofil = "https://cdn1.iconfinder.com/data/icons/ionicons-sharp-vol-2/512/person-circle-sharp-1024.png"
 }
